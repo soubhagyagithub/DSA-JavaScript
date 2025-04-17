cacca6acd3f87da71fea0e78b3a0b71d039634e0
Soubhagya Ranjan Sethi <93478954+soubhagyagithub@users.noreply.github.com>
2025-04-17 11:45:24 +0530

    Updated min.js

--- array/Easy/min.js
+++ array/Easy/min.js
@@ -3,7 +3,7 @@
 // Steps are:
 // 1. Initializing the temp value to maximum value .
 // 2. Take a loop and iterating each value in the array and checking the temp value with current value wheather current value is small or not than temp value .
-// 3. when current value is smaller than temp value then store current value to temp and keep doing until get minimum value.
+// 3. when current value is smaller than temp value then store current value to temp and keep doing until get minimum value.and then return temp
 function minElement(nums) {
   let min = Infinity;
   for (let i = 0; i < nums.length; i++) {
